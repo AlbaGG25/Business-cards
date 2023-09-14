3dfa0ff21023fc6b8305e7aa506cba5edf0567fe
Tania <taniie_bcn@hotmail.es>
2023-09-14 18:28:18 +0200

    added btn reset js

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,43 @@
 'use strict';
 
 /*1- boton reset
-darle Clase x
-llamar al elemento dqs x
-hacer variable x
-click evento para borrar
-
-
-2- collapsable (1ª siempre abierta)
-darle clase
-llamar al elemento y darle variable dqs
-click evento cerrar otras opciones
-
-
+-evento 
+2- enlazar los inputs del formulario 
+-con los iconos de la card
+-con nombre y puesto
+3-paleta de colores
+-evento para cambiar de color
+4- crear evento para que al crear tarjeta se abra compartir en X
 */
 
-/*const btnReset = document.querySelector ('.js-reset');
+//variables imput fomulario
+const nameUser = document.querySelector('.js-name');
+const job = document.querySelector('.js-job');
+const img = document.querySelector('.js-img');
+const email = document.querySelector('.js-email');
+const mobile = document.querySelector('.js-mobile');
+const lk = document.querySelector('.js-lk');
+const git = document.querySelector('.js-git');
+
+
+//Boton reset
+const btnReset = document.querySelector ('.js-reset');
 
 function resetClick (){
-
+  event.preventDefault();
+  nameUser.value='';
+  job.value='';  
+  img.value='';
+  email.value='';
+  mobile.value='';
+  lk.value='';
+  git.value='';
 }
 
+btnReset.addEventListener('click', resetClick);
 
-btnReset.addEventListener('click', resetClick);*/
 
+//variables collapsables
 const fieldsetDesign = document.querySelector('.js-fieldset_design');
 const fieldsetFill = document.querySelector('.js-fieldset_fill');
 const fieldsetShare = document.querySelector('.js-fieldset_share');
